167b71879a1cc7e840526ad1a57550d8af239488
MaiThiHongNgoc <hongngoc200509@gmail.com>
2025-06-04 15:22:38 +0700

    qr oki

--- src/main/java/org/example/sem4backend/repository/QRAttendanceRepository.java
+++ src/main/java/org/example/sem4backend/repository/QRAttendanceRepository.java
@@ -1,6 +1,5 @@
 package org.example.sem4backend.repository;
 
-import org.example.sem4backend.entity.Employee;
 import org.example.sem4backend.entity.QRAttendance;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,6 +7,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface QRAttendanceRepository extends JpaRepository<QRAttendance, String> {
-    Optional<QRAttendance> findTopByEmployeeOrderByScanTimeDesc(Employee employee);
     List<QRAttendance> findByActiveStatus(QRAttendance.ActiveStatus activeStatus);
+
 }
