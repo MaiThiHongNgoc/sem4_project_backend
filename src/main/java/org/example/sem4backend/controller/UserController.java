9c5b6784edbf4d9615ae0e604c92e090574f41b6
hoanguyen24072005 <nduc37039@gmail.com>
2025-06-19 15:03:37 +0700

    up

--- src/main/java/org/example/sem4backend/controller/UserController.java
+++ src/main/java/org/example/sem4backend/controller/UserController.java
@@ -82,7 +82,7 @@ public class UserController {
         ApiResponse<Void> response = userService.deleteUser(userId);
         return new ResponseEntity<>(response, response.isSuccess() ? HttpStatus.OK : HttpStatus.BAD_REQUEST);
     }
-    
+
     @PreAuthorize("hasAnyRole('Admin', 'Hr')")
     @GetMapping("/native")
     public ResponseEntity<ApiResponse<List<UserResponse>>> getAllUsersNative() {
